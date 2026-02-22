91f4f43fbac7a03a7d234b6d4641a754059a5614
Yuliia <semockoula@gmail.com>
2026-02-22 23:15:08 +0200

    rename controller getNotes to getAllNotes

--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -3,13 +3,13 @@ import {
   createNote,
   deleteNote,
   getNoteById,
-  getNotes,
+  getAllNotes,
   updateNote,
 } from '../controllers/notesController.js';
 
 const router = Router();
 
-router.get('/notes', getNotes);
+router.get('/notes', getAllNotes);
 router.get('/notes/:noteId', getNoteById);
 router.post('/notes', createNote);
 router.delete('/notes/:noteId', deleteNote);
