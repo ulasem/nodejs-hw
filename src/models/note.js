06b3e118ba228c0db42759587bfae908e38fba4f
Yuliia <semockoula@gmail.com>
2026-03-01 17:50:45 +0200

    fix tags import in note model

--- src/models/note.js
+++ src/models/note.js
@@ -1,32 +1,20 @@
-import { Schema, model } from 'mongoose';
+import mongoose from 'mongoose';
+import { TAGS } from '../constants/tags.js';
 
-const noteSchema = new Schema(
+const noteSchema = new mongoose.Schema(
   {
     title: {
       type: String,
       required: true,
-      trim: true,
+      minlength: 1,
     },
     content: {
       type: String,
       default: '',
-      trim: true,
     },
     tag: {
       type: String,
-      enum: [
-        'Work',
-        'Personal',
-        'Meeting',
-        'Shopping',
-        'Ideas',
-        'Travel',
-        'Finance',
-        'Health',
-        'Important',
-        'Todo',
-      ],
-      default: 'Todo',
+      enum: TAGS,
     },
   },
   {
@@ -36,4 +24,4 @@ const noteSchema = new Schema(
 
 noteSchema.index({ title: 'text', content: 'text' });
 
-export const Note = model('Note', noteSchema);
+export const Note = mongoose.model('Note', noteSchema);
