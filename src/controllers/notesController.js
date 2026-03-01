5534037e909dc0873f62542bcec3df5769e71945
Yuliia <semockoula@gmail.com>
2026-03-01 17:53:07 +0200

    fix getAllNotes

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -16,9 +16,10 @@ export const getAllNotes = async (req, res) => {
     filter.$text = { $search: search };
   }
 
-  const totalNotes = await Note.countDocuments(filter);
-
-  const notes = await Note.find(filter).skip(skip).limit(perPage);
+  const [totalNotes, notes] = await Promise.all([
+    Note.countDocuments(filter),
+    Note.find(filter).skip(skip).limit(perPage),
+  ]);
 
   const totalPages = Math.ceil(totalNotes / perPage);
 
