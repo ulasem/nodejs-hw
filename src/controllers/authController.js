0823faedd6f1b4c7b3d1af198d5391774aa0d6b4
Yuliia <semockoula@gmail.com>
2026-03-14 14:38:26 +0200

    fix mistakes

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -7,7 +7,7 @@ import jwt from 'jsonwebtoken';
 import { User } from '../models/user.js';
 import { createSession, setSessionCookies } from '../services/auth.js';
 import { Session } from '../models/session.js';
-import { sendMail } from '../utils/sendMail.js';
+import { sendEmail } from '../utils/sendMail.js';
 
 export const registerUser = async (req, res) => {
   const { email, password } = req.body;
@@ -102,7 +102,7 @@ export const requestResetEmail = async (req, res) => {
   const user = await User.findOne({ email });
   if (!user) {
     return res.status(200).json({
-      message: 'If this email exists, a reset link has been sent',
+      message: 'Password reset email sent successfully',
     });
   }
 
@@ -126,7 +126,7 @@ export const requestResetEmail = async (req, res) => {
   });
 
   try {
-    await sendMail({
+    await sendEmail({
       from: process.env.SMTP_FROM,
       to: email,
       subject: 'Reset your password',
