63b9d51953bc6714e08733e80a88680fe989497f
Yuliia <semockoula@gmail.com>
2026-03-14 10:30:12 +0200

    h-w 5

--- src/server.js
+++ src/server.js
@@ -10,6 +10,7 @@ import { errorHandler } from './middleware/errorHandler.js';
 
 import noteRoutes from './routes/notesRoutes.js';
 import authRoutes from './routes/authRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -21,6 +22,7 @@ app.use(cookieParser());
 
 app.use(authRoutes);
 app.use(noteRoutes);
+app.use(userRoutes);
 
 app.use(notFoundHandler);
 app.use(errors());
